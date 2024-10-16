6d7808bbef0e2b418ce33fc722b879df593f4975
Nhahan <kisy324@naver.com>
2024-10-16 17:50:35 +0900

    remove useless '@Repository' annotation

--- src/main/java/org/example/springlock/distributed/DistributedCounterRepository.java
+++ src/main/java/org/example/springlock/distributed/DistributedCounterRepository.java
@@ -1,8 +1,6 @@
 package org.example.springlock.distributed;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface DistributedCounterRepository extends JpaRepository<DistributedCounter, Long> {
 }
--- src/main/java/org/example/springlock/pessimistic/PessimisticCounterRepository.java
+++ src/main/java/org/example/springlock/pessimistic/PessimisticCounterRepository.java
@@ -4,11 +4,9 @@ import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface PessimisticCounterRepository extends JpaRepository<PessimisticCounter, Long> {
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
